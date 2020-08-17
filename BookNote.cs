ad30a0c06bd0268d557e18d76511eabb46db3408
Raphael Ramos <rrs@brickabode.com>
2020-08-17 13:15:48 -0300

    Add Controller and base Model

---
+++ BookNote.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Papyrus_API
+{
+    public class BookNote
+    {
+        public int Id { get; set; }
+        public int ImageId { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+
+    }
+}
---
+++ Controllers/BookNoteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Papyrus_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookNoteController : ControllerBase
+    {
+        private readonly ILogger<BookNoteController> _logger;
+
+        public BookNoteController(ILogger<BookNoteController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
